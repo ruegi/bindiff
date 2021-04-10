eadf10cf8bbe549d7bd0b652e932f7fcd4bcca16
ruegi <rg@rgilbert.de>
2021-04-10 17:43:33 +0200

    Kleine optische Verbeserungen; auf Github abgelegt

--- bindiff.py
+++ bindiff.py
@@ -3,6 +3,7 @@ bindiff.py
 Vergleicht zwei Ordner binär
 
 rg, ab 2020-07-03
+    2021-01-07 ANSI Sequenzen aktiviert
 
 """
 
@@ -15,13 +16,22 @@ from pathlib import Path
 
 from typing import List
 
+# Dictionary, um ANSI-Sequenzen in der Ausgabe zu kapseln
+COLOR = {
+    "HEADER": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+}
+
 # primPath = "/mnt/platte/schnitt"
 # secPath = "/archiv/video/_in4"
 
-primPath = "E:/Filme/schnitt.alt"
-secPath = "y:/video/_in8"
+primPath = "E:/Filme/schnitt"
+secPath = "y:/video/_in"
 
-# erster Test: alle Dateien, die im primPath sind, müssen auch im secPath sein
+version = "1.2 vom 2021-04-10"
 
 class Ergebnis():
     def __init__(self):
@@ -31,11 +41,8 @@ class Ergebnis():
         self.binfehler: int = 0
         self.fList: List = [] 
 
-    def __str__(self):
-        if self.fList == 0:
-            return f"{self.gesamt} geprüft: davon {self.ok} OK; {self.fehler} fehlerhaft")    
-        else:
-            return f"{self.gesamt} geprüft: davon {self.ok} OK; {self.fehler} fehlerhaft\n Fehlerhafte Binärvergleiche:\n" + "\n".join(self.fList)
+    def __str__(self):        
+        return f"{self.gesamt} geprüft: davon {self.ok} OK; {self.fehler} fehlerhaft\n Fehlrehafte Binärvergleiche:\n" + "\n".join(self.fList)
     
     def adderr(self, ftext):
         self.fList.append(ftext)
@@ -58,11 +65,15 @@ def progress(count, total, status='', bar_len=40):
 def bin_vgl(file1: str, file2: str, flen: int) -> bool:
     global result
     name = file1[file1.rfind("/")+1:]
+    txtwidth = os.get_terminal_size().columns
+    maxnamew = txtwidth - 75
+    if len(name) > maxnamew:
+        name = name[0:maxnamew] + '...'
     # führt einen binären Vergleich der beiden Dateien zurück;  
     # bei Übereinstimmung wird True zurückgegeben, sosnt false
     buflen = 32*1024*1024    # 32 MB
     isOK = True    
-    with open(file1, "rb") as f1, open(file2, "rb") as f2: # progressbar.ProgressBar(max_value=100, redirect_stdout=True) as bar:
+    with open(file1, "rb") as f1, open(file2, "rb") as f2:          # progressbar.ProgressBar(max_value=100, redirect_stdout=True) as bar:
         # print(f">> {name} ... binärer Vergleich .. ", end="", flush=True)
         # print(f">> {name} ... binärer Vergleich .. ")
         lenleft = flen
@@ -98,12 +109,18 @@ def bin_vgl(file1: str, file2: str, flen: int) -> bool:
 prim_path: Path = Path(primPath)
 sec_path: Path = Path(secPath)
 
+# StartMessage
+os.system('')   # magic Call to enable ANSi-Seq.
+print("=" * 80)
+print(COLOR["BLUE"] + 'BinDiff' + COLOR["ENDC"] + ' by ruegi,')
+print(COLOR["BLUE"] + f'Version: {version}' + COLOR["ENDC"])
+print("=" * 80)
+
 for p_root, p_dirs, p_files in os.walk(prim_path):
     prim_root = p_root
     prim_files = p_files
     break
 
-# sec_root, sec_dirs, sec_files = os.walk(sec_path)
 result = Ergebnis()
 
 for name in prim_files:
@@ -111,19 +128,21 @@ for name in prim_files:
     prim_file = os.path.join(prim_root, name)
     sec_file = os.path.join(sec_path, name)
     if os.path.isfile(sec_file):
+        # erster Test: alle Dateien, die im primPath sind, müssen auch im secPath und längengleich sein
         prim_stats = os.stat(prim_file)
         sec_stats = os.stat(sec_file)
-        if prim_stats.st_size == sec_stats.st_size:
-            print(f"> {name} ... Existiert und ist längengleich .. OK\n")
-            if bin_vgl(prim_file, sec_file, prim_stats.st_size):
+        if prim_stats.st_size == sec_stats.st_size:            
+            print(">" + COLOR["GREEN"] + f"{name}" + COLOR["ENDC"] + " ... Existiert und ist längengleich .. OK\n")
+            # zweiter Test: binärer Vergleich
+            if bin_vgl(prim_file, sec_file, prim_stats.st_size):                
                 result.ok += 1
             else:
                 result.fehler += 1
         else:
-            print(f"> {name} ... {prim_stats.st_size()} <=ne=> {sec_stats.st_size()}")
+            print("> " + COLOR["RED"] + f"{name} ... {prim_stats.st_size()} <=ne=> {sec_stats.st_size()}")
             result.fehler += 1
     else:
-        print(f">>> {sec_file} ... FEHLT")
+        print(">>> " + COLOR["RED"] + f"{sec_file} ... FEHLT")
         result.fehler += 1
 
 print(result)
